91475c82cb1bd4733f58383cc53cad07e843d057
xueyongwei <xueyongwei@eoemarket.com>
2017-08-26 20:35:04 +0800

    Initial Commit

---
+++ Recipes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Recipes
+//
+//  Created by 薛永伟 on 2017/8/26.
+//  Copyright © 2017年 XYW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Recipes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Recipes
+//
+//  Created by 薛永伟 on 2017/8/26.
+//  Copyright © 2017年 XYW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
